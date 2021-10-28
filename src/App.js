7f12376ea94da13ebc929839080e56bc14576f60
duncanlaidlaw <slamdunc123@gmail.com>
2021-10-28 15:15:50 +0100

    add api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [data, setData] = useState(null);
+
+	useEffect(() => {
+		fetch('https://basic-express-api.herokuapp.com/players')
+			.then((res) => res.json())
+			.then((data) => setData(data));
+	}, []);
+
+	return (
+		<div className='App'>
+            {!data ? 'Loading...' : data.map((item)=> <h5>{item.name}</h5>)}
+		</div>
+	);
 }
 
 export default App;
