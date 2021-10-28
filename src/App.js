58f2795e7d63b5a701658863b1d490cf7a605c82
duncanlaidlaw <slamdunc123@gmail.com>
2021-10-28 15:36:26 +0100

    add title

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
 
 	return (
 		<div className='App'>
+            <h3>React front end hosted on Netlify connecting to Express backend hosted on Heroku</h3>
             {!data ? 'Loading...' : data.map((item)=> <h5>{item.name}</h5>)}
 		</div>
 	);
